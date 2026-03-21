59804f2b5d913e403f922b2e5663201efaf2d73a
Kolyn090 <kolynlin@protonmail.com>
2026-03-21 03:58:53 -0400

    Batch organize pmo & tmh to model

--- psp/organize_process/batch_organize.py
+++ psp/organize_process/batch_organize.py
@@ -4,19 +4,24 @@ import shutil
 import argparse
 from pathlib import Path
 
-def organize_obj_mtl(ofp, tfp, new_save_path):
+def organize_obj_mtl(ofp, tfp, new_save_path, pmo_code, tmh_code):
     folder = Path(ofp)
     obj_files = list(folder.glob("*.obj"))
 
     for obj_file in obj_files:
+        # print(obj_file)
         basename = os.path.basename(obj_file)
+        if pmo_code not in basename:
+            continue
         name = basename.replace(".obj", "")
 
         save_folder_path = os.path.join(new_save_path, name)
+        save_folder_path = os.path.join(save_folder_path, tmh_code)
         os.makedirs(save_folder_path, exist_ok=True)
 
         # Copy OBJ
         new_obj_path = os.path.join(save_folder_path, basename)
+        # print(new_obj_path)
         shutil.copy(obj_file, new_obj_path)
 
         # ✅ Fix mtllib
@@ -29,7 +34,7 @@ def organize_obj_mtl(ofp, tfp, new_save_path):
             f.write(text)
 
         # Copy texture folder
-        target_mtl_folder = os.path.join(tfp, name)
+        target_mtl_folder = os.path.join(tfp, name.replace(f"-{pmo_code}", f"-{tmh_code}"))
         texture_dest = os.path.join(save_folder_path, "texture")
 
         if os.path.exists(target_mtl_folder):
@@ -65,11 +70,15 @@ if __name__ == '__main__':
     parser.add_argument('pmo_folder')
     parser.add_argument('tmh_folder')
     parser.add_argument('savefolder')
+    parser.add_argument('pmo_code')
+    parser.add_argument('tmh_code')
     args = parser.parse_args()
 
     pmo_folder = args.pmo_folder
     tmh_folder = args.tmh_folder
     savefolder = args.savefolder
+    pmo_code = args.pmo_code
+    tmh_code = args.tmh_code
 
     os.makedirs(savefolder, exist_ok=True)
-    organize_obj_mtl(pmo_folder, tmh_folder, savefolder)
+    organize_obj_mtl(pmo_folder, tmh_folder, savefolder, pmo_code, tmh_code)
