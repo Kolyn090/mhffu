3928562e3219ceeddda7c434321fdf108234bac6
Kolyn090 <kolynlin@protonmail.com>
2026-03-21 01:49:56 -0400

    Batch tmh process

--- psp/tmh_process/batch_tmh.py
+++ psp/tmh_process/batch_tmh.py
@@ -3,22 +3,54 @@ import argparse
 from pathlib import Path
 from tmh import convert_tmh
 
-def batch_tmh(folder_path, new_save_path):
+def batch_tmh(folder_path, new_save_path, tmh1_code, tmh2_code):
     folder = Path(folder_path)
     pac_files = list(folder.glob("*.pac"))
-    pmo_files = []
-    mtl_files = []
+    tmh_ext_files = list(folder.glob("*.tmh"))
+    pac_files.extend(tmh_ext_files)
     tmh_files = []
+    tmh_files1 = []
+    tmh_files2 = []
+
     for f in pac_files:
         basename = os.path.basename(f)
-        if "0000" in basename:
-            pmo_files.append(f)
-        elif "0001" in basename:
-            mtl_files.append(f)
-        elif "0002" in basename:
-            tmh_files.append(f)
+        if ".tmh" in basename:
+            tmh_file.append(f)
+        if tmh1_code and tmh1_code in basename:
+            tmh_files1.append(f)
+        if tmh2_code and tmh2_code in basename:
+            tmh_files2.append(f)
+
     for tmh_file in tmh_files:
-        basename = os.path.basename(tmh_file).replace("-0002.pac", "")
+        basename = os.path.basename(tmh_file).replace(f".tmh", "")
+        # folder
+        save_folder_path = os.path.join(new_save_path, basename)
+        os.makedirs(save_folder_path, exist_ok=True)
+        
+        # file
+        mtl_path = os.path.join(save_folder_path, "material.mtl")
+
+        try:
+            convert_tmh(tmh_file, mtl_path)
+        except Exception as e:
+            print(f"Failed to process {tmh_file}")
+
+    for tmh_file in tmh_files1:
+        basename = os.path.basename(tmh_file).replace(f"-{tmh1_code}.pac", f"-{tmh1_code}")
+        # folder
+        save_folder_path = os.path.join(new_save_path, basename)
+        os.makedirs(save_folder_path, exist_ok=True)
+        
+        # file
+        mtl_path = os.path.join(save_folder_path, "material.mtl")
+
+        try:
+            convert_tmh(tmh_file, mtl_path)
+        except Exception as e:
+            print(f"Failed to process {tmh_file}")
+
+    for tmh_file in tmh_files2:
+        basename = os.path.basename(tmh_file).replace(f"-{tmh2_code}.pac", f"-{tmh2_code}")
         # folder
         save_folder_path = os.path.join(new_save_path, basename)
         os.makedirs(save_folder_path, exist_ok=True)
@@ -35,10 +67,14 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('inputfolder')
     parser.add_argument('outputfolder')
+    parser.add_argument('--tmh1_code', required=False)
+    parser.add_argument('--tmh2_code', required=False)
     args = parser.parse_args()
 
     inputfolder = args.inputfolder
     outputfolder = args.outputfolder
+    tmh1_code = args.tmh1_code
+    tmh2_code = args.tmh2_code
 
     os.makedirs(outputfolder, exist_ok=True)
-    batch_tmh(inputfolder, outputfolder)
+    batch_tmh(inputfolder, outputfolder, tmh1_code, tmh2_code)
--- psp/tmh_process/batch_tmh.sh
+++ psp/tmh_process/batch_tmh.sh
@@ -1,4 +1,114 @@
+inputfolder="../export/extract/cat/"
+outputfolder="../export/tmh/cat/"
+echo "Process $inputfolder -> $outputfolder"
+python3 batch_tmh.py "$inputfolder" "$outputfolder" --tmh1_code "0002"
+
+inputfolder="../export/extract/edit/"
+outputfolder="../export/tmh/edit/"
+echo "Process $inputfolder -> $outputfolder"
+python3 batch_tmh.py "$inputfolder" "$outputfolder" --tmh1_code "0002"
+
+inputfolder="../export/extract/effect/"
+outputfolder="../export/tmh/effect/"
+echo "Process $inputfolder -> $outputfolder"
+python3 batch_tmh.py "$inputfolder" "$outputfolder" --tmh1_code "0002"
+
+inputfolder="../export/extract/emmodel/"
+outputfolder="../export/tmh/emmodel/"
+echo "Process $inputfolder -> $outputfolder"
+python3 batch_tmh.py "$inputfolder" "$outputfolder" --tmh1_code "0002" --tmh2_code "0006"
+
+inputfolder="../export/extract/npc/"
+outputfolder="../export/tmh/npc/"
+echo "Process $inputfolder -> $outputfolder"
+python3 batch_tmh.py "$inputfolder" "$outputfolder" --tmh1_code "0002"
+
 inputfolder="../export/extract/player/f/arm/"
 outputfolder="../export/tmh/player/f/arm/"
+echo "Process $inputfolder -> $outputfolder"
+python3 batch_tmh.py "$inputfolder" "$outputfolder" --tmh1_code "0002"
 
-python3 batch_tmh.py "$inputfolder" "$outputfolder"
+inputfolder="../export/extract/player/f/body/"
+outputfolder="../export/tmh/player/f/body/"
+echo "Process $inputfolder -> $outputfolder"
+python3 batch_tmh.py "$inputfolder" "$outputfolder" --tmh1_code "0002"
+
+inputfolder="../export/extract/player/f/face/"
+outputfolder="../export/tmh/player/f/face/"
+echo "Process $inputfolder -> $outputfolder"
+python3 batch_tmh.py "$inputfolder" "$outputfolder" --tmh1_code "0002"
+
+inputfolder="../export/extract/player/f/hair/"
+outputfolder="../export/tmh/player/f/hair/"
+echo "Process $inputfolder -> $outputfolder"
+python3 batch_tmh.py "$inputfolder" "$outputfolder" --tmh1_code "0002"
+
+inputfolder="../export/extract/player/f/head/"
+outputfolder="../export/tmh/player/f/head/"
+echo "Process $inputfolder -> $outputfolder"
+python3 batch_tmh.py "$inputfolder" "$outputfolder" --tmh1_code "0002"
+
+inputfolder="../export/extract/player/f/reg/"
+outputfolder="../export/tmh/player/f/reg/"
+echo "Process $inputfolder -> $outputfolder"
+python3 batch_tmh.py "$inputfolder" "$outputfolder" --tmh1_code "0002"
+
+inputfolder="../export/extract/player/f/skin/"
+outputfolder="../export/tmh/player/f/skin/"
+echo "Process $inputfolder -> $outputfolder"
+python3 batch_tmh.py "$inputfolder" "$outputfolder" --tmh1_code "0002"
+
+inputfolder="../export/extract/player/f/wst/"
+outputfolder="../export/tmh/player/f/wst/"
+echo "Process $inputfolder -> $outputfolder"
+python3 batch_tmh.py "$inputfolder" "$outputfolder" --tmh1_code "0002"
+
+inputfolder="../export/extract/player/m/arm/"
+outputfolder="../export/tmh/player/m/arm/"
+echo "Process $inputfolder -> $outputfolder"
+python3 batch_tmh.py "$inputfolder" "$outputfolder" --tmh1_code "0002"
+
+inputfolder="../export/extract/player/m/body/"
+outputfolder="../export/tmh/player/m/body/"
+echo "Process $inputfolder -> $outputfolder"
+python3 batch_tmh.py "$inputfolder" "$outputfolder" --tmh1_code "0002"
+
+inputfolder="../export/extract/player/m/face/"
+outputfolder="../export/tmh/player/m/face/"
+echo "Process $inputfolder -> $outputfolder"
+python3 batch_tmh.py "$inputfolder" "$outputfolder" --tmh1_code "0002"
+
+inputfolder="../export/extract/player/m/hair/"
+outputfolder="../export/tmh/player/m/hair/"
+echo "Process $inputfolder -> $outputfolder"
+python3 batch_tmh.py "$inputfolder" "$outputfolder" --tmh1_code "0002"
+
+inputfolder="../export/extract/player/m/head/"
+outputfolder="../export/tmh/player/m/head/"
+echo "Process $inputfolder -> $outputfolder"
+python3 batch_tmh.py "$inputfolder" "$outputfolder" --tmh1_code "0002"
+
+inputfolder="../export/extract/player/m/reg/"
+outputfolder="../export/tmh/player/m/reg/"
+echo "Process $inputfolder -> $outputfolder"
+python3 batch_tmh.py "$inputfolder" "$outputfolder" --tmh1_code "0002"
+
+inputfolder="../export/extract/player/m/skin/"
+outputfolder="../export/tmh/player/m/skin/"
+echo "Process $inputfolder -> $outputfolder"
+python3 batch_tmh.py "$inputfolder" "$outputfolder" --tmh1_code "0002"
+
+inputfolder="../export/extract/player/m/wst/"
+outputfolder="../export/tmh/player/m/wst/"
+echo "Process $inputfolder -> $outputfolder"
+python3 batch_tmh.py "$inputfolder" "$outputfolder" --tmh1_code "0002"
+
+inputfolder="../export/extract/stage/"
+outputfolder="../export/tmh/stage/"
+echo "Process $inputfolder -> $outputfolder"
+python3 batch_tmh.py "$inputfolder" "$outputfolder" --tmh1_code "0001"
+
+inputfolder="../export/extract/weapon/"
+outputfolder="../export/tmh/weapon/"
+echo "Process $inputfolder -> $outputfolder"
+python3 batch_tmh.py "$inputfolder" "$outputfolder" --tmh1_code "0002"
