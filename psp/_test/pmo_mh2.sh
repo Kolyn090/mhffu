fb341a42f7de614017d45692c119559a06f2642a
Kolyn090 <kolynlin@protonmail.com>
2026-03-23 22:08:06 -0400

    Make better testing script

--- psp/_test/pmo_mh2.sh
+++ psp/_test/pmo_mh2.sh
@@ -1,6 +1,32 @@
 clear
-pmo_file="../export/extract/emmodel/em01-0001.pac"
-tmh_file="../export/extract/emmodel/em01-0002.pac"
-outputfile="../export/test/em01-0001.obj"
-# python3 ../pmo_process/pmo_new.py "$pmo_file" "$tmh_file" "$outputfile" --enforce_ge_verbose
-python3 ../pmo_process/pmo_new.py "$pmo_file" "$tmh_file" "$outputfile" --verbose
+# For testing convenience
+monster_num="06"
+pmo_file="../export/extract/emmodel/em$monster_num-0001.pac"
+tmh_file="../export/extract/emmodel/em$monster_num-0002.pac"
+material_folder="../export/tmh/emmodel/em$monster_num-0002/"
+output_file="../export/test/em$monster_num-0001.obj"
+
+output_folder="../export/test/"
+
+echo "Cleaning up output folder..."
+[ -n "$output_folder" ] && rm -rf "$output_folder"/* "$output_folder"/.[!.]* "$output_folder"/..?*
+mkdir -p "$output_folder"
+
+echo "Creating texture folder..."
+mkdir -p "$output_folder"/texture
+
+echo "Copying mtl file..."
+file=$(find "$material_folder" -type f -name '*mtl' -print -quit)
+if [ -n "$file" ]; then
+    cp "$file" "$output_folder"
+else
+    echo "No .mtl file found"
+fi
+
+echo "Copying textures..."
+shopt -s nullglob
+files=("$material_folder"/*.png)
+[ ${#files[@]} -gt 0 ] && cp "${files[@]}" "$output_folder"/texture
+
+# python3 ../pmo_process/pmo_new.py "$pmo_file" "$tmh_file" "$output_file" --enforce_ge_verbose
+python3 ../pmo_process/pmo_new.py "$pmo_file" "$tmh_file" "$output_file" --verbose
