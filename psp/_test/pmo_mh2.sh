e1cdf9c413302df3b7f6badbc172fe30a3e5d558
Kolyn090 <kolynlin@protonmail.com>
2026-03-23 21:29:42 -0400

    I'm thinking remove mesh header

--- psp/_test/pmo_mh2.sh
+++ psp/_test/pmo_mh2.sh
@@ -1,6 +1,6 @@
 clear
-pmo_file="../export/extract/emmodel/em01-0001.pac"
-tmh_file="../export/extract/emmodel/em01-0002.pac"
-outputfile="../export/test/em01-0001.obj"
+pmo_file="../export/extract/emmodel/em03-0001.pac"
+tmh_file="../export/extract/emmodel/em03-0002.pac"
+outputfile="../export/test/em03-0001.obj"
 # python3 ../pmo_process/pmo_new.py "$pmo_file" "$tmh_file" "$outputfile" --enforce_ge_verbose
 python3 ../pmo_process/pmo_new.py "$pmo_file" "$tmh_file" "$outputfile" --verbose
--- psp/pmo_process/convert.py
+++ psp/pmo_process/convert.py
@@ -59,23 +59,31 @@ def debug_pmo_header(raw, pmo_header, extra_indent):
 
     Logger.debug("=== PMO HEADER ===", extra_indent)
     Logger.highlight("1 Unsigned Int", extra_indent)
-    Logger.debug(f"[0] file_size    = {unsigned_int_hex(pmo_header[0])}", extra_indent)
+    Logger.debug(f"[0]  file_size                                = {unsigned_int_hex(pmo_header[0])}", extra_indent)
 
     Logger.highlight("4 Floats", extra_indent)
-    Logger.debug(f"[1] width?       = {pmo_header[1]}", extra_indent)
-    Logger.debug(f"[2-4] size       = {scale}", extra_indent)
+    Logger.debug(f"[1] width?                                    = {pmo_header[1]} (unused)", extra_indent)
+    Logger.debug(f"[2] size x                                    = {pmo_header[2]}", extra_indent)
+    Logger.debug(f"[3] size y                                    = {pmo_header[3]}", extra_indent)
+    Logger.debug(f"[4] size z                                    = {pmo_header[4]}", extra_indent)
 
     Logger.highlight("2 Unsigned Shorts", extra_indent)
-    Logger.debug(f"[5] i-loop len   = {pmo_header[5]}", extra_indent)
-    Logger.debug(f"[6] unknown      = {pmo_header[6]}", extra_indent)
+    Logger.debug(f"[5] No idea                                   = {pmo_header[5]} (unused)", extra_indent)
+    Logger.debug(f"[6] i-loop len                                = {pmo_header[6]} (unused)", extra_indent)
+    hint1 = "↑ Note: use determine_i_len() instead"
+    Logger.debug(hint1, extra_indent)
 
     Logger.highlight("8 Unsigned Ints", extra_indent)
-    Logger.debug(f"[7] mesh_table   = {unsigned_int_hex(pmo_header[7])}", extra_indent)
-    Logger.debug(f"[8] group_table  = {unsigned_int_hex(pmo_header[8])}", extra_indent)
-    Logger.debug(f"[9] ?            = {unsigned_int_hex(pmo_header[9])}", extra_indent)
-    Logger.debug(f"[10] ?           = {unsigned_int_hex(pmo_header[10])}", extra_indent)
-    Logger.debug(f"[11] vertex_buf  = {unsigned_int_hex(pmo_header[11])}", extra_indent)
-    Logger.debug(f"[12-14] ?        = {pmo_header[12:15]}", extra_indent)
+    Logger.debug(f"[7]  Mesh Table begin (0x20)  = {unsigned_int_hex(pmo_header[7])}", extra_indent)
+    Logger.debug(f"[8]  Vertex group header begin = {unsigned_int_hex(pmo_header[8])}", extra_indent)
+    hint2 = "↑ Note: each vertex group header is 0x10, the last vertex group header should be directly above pmo_header[9]"
+    Logger.debug(hint2, extra_indent)
+    Logger.debug(f"[9]  Material index map table begin (I think?) = {unsigned_int_hex(pmo_header[9])}", extra_indent)
+    Logger.debug(f"[10] Material index map table end (I think?)   = {unsigned_int_hex(pmo_header[10])}", extra_indent)
+    Logger.debug(f"[11] Material index table  = {unsigned_int_hex(pmo_header[11])}", extra_indent)
+    Logger.debug(f"[12] Begin of the first GE region              = {pmo_header[12]}", extra_indent)
+    Logger.debug(f"[13] Trash?                                    = {pmo_header[13]} (unused)", extra_indent)
+    Logger.debug(f"[14] Trash?                                    = {pmo_header[14]} (unused)", extra_indent)
 
     Logger.newline()
 
@@ -113,7 +121,7 @@ def debug_mesh_header(mesh_header, extra_indent):
     Logger.highlight("4 Unsigned Shorts", extra_indent)
     Logger.debug(f"[4]  = {mesh_header[4]}", extra_indent)
     Logger.debug(f"[5]  = {mesh_header[5]}", extra_indent)
-    Logger.debug(f"[6]  = {mesh_header[6]} (Length of J-loop)", extra_indent)
+    Logger.debug(f"[6]  = {mesh_header[6]}", extra_indent)
     Logger.debug(f"[7]  = {mesh_header[7]}", extra_indent)
 
     Logger.highlight("2 Unsigned Ints", extra_indent)
@@ -138,9 +146,19 @@ def debug_vertex_group_raw(raw_v, extra_indent):
 
 def debug_vertex_group_header(vertex_group_header, extra_indent):
     Logger.debug("=== Vertex Group HEADER ===", extra_indent)
-    for i, val in enumerate(vertex_group_header):
-        Logger.debug(f"[{i}]  = {val}", extra_indent)
 
+    Logger.highlight("2 Unsigned Chars", extra_indent)
+    Logger.debug(f"[1] Vertex group flag (?) = {vertex_group_header[1]}", extra_indent)
+    Logger.debug(f"[2] No idea               = {vertex_group_header[2]} (unused)", extra_indent)
+
+    Logger.highlight("1 Unsigned Short", extra_indent)
+    Logger.debug(f"[3] No idea               = {vertex_group_header[3]} (unused)", extra_indent)
+
+    Logger.highlight("3 Unsigned Ints", extra_indent)
+    Logger.debug(f"[4] Vertex offset         = {vertex_group_header[4]} (unused)", extra_indent)
+    hint1 = "↑ Note: add pmo_header[12] is the GE region"
+    Logger.debug(hint1, extra_indent)
+    Logger.debug(f"[5] No idea               = {vertex_group_header[5]} (unused)", extra_indent)
 
 # =========================
 # CORE LOGIC (UNCHANGED)
@@ -175,104 +193,6 @@ def read_vertex_group(pmo, offset, extra_indent: int):
 # MAIN FUNCTION
 # =========================
 
-def convert_mh2_pmo2(pmo, obj, mtl_file, second=None, verbose=False, enforce_ge_verbose=False):
-    Logger.enable = verbose
-    # Logger.set_log_file("log.txt")
-
-    Logger.debug(f"Current Pos: {pmo.tell():08X}")
-
-    pmo_header = read_pmo_header(pmo, 0)
-    scale = pmo_header[2:5]
-
-    # Avoid long debug info
-    has_dbg_ge_once = False
-
-    dirname = os.path.dirname(obj.name)
-    basename = os.path.basename(obj.name)
-    count = 0
-
-    #        0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20
-    jles = [15, 5, 1, 1,11,12, 3, 5, 3, 1, 7, 1,21, 1, 1, 2, 5, 4,26, 2,120]
-    mats = [ 3, 1, 1 ,1, 2, 2, 1, 0, 3, 3, 3, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0]
-
-    curr_header = -100000
-    for i in range(pmo_header[6]):
-        # if i >= len(jles):
-        #     continue
-
-        Logger.highlight(f"I-Loop ({i+1} / {pmo_header[5]})", 0, color=LogStyle.BRIGHT_MAGENTA)
-        mesh = []
-        Logger.highlight("Mesh Header: ", indent=1, color=LogStyle.GREEN)
-        mesh_header = read_mesh_header(pmo, pmo_header[7], 1)
-
-        # j_len = jles[i]
-
-        for j in range(j_len):
-            Logger.highlight(f"J-Loop ({j+1} / {j_len}) in i:{i+1}", 1, color=LogStyle.RED)
-
-            Logger.highlight("Vertex Group Header: ", indent=2, color=LogStyle.GREEN)
-
-            vg_offset = pmo_header[8] + ((mesh_header[7] + count) * 0x10)
-            Logger.debug(f"{pmo_header[8]:08X} + (({mesh_header[7]:08X} + {count}) * 0x10)", 2)
-            vertex_group_header = read_vertex_group(pmo, vg_offset, 2)
-
-            Logger.highlight("Vertex Data (PMO  ): ", indent=2, color=LogStyle.GREEN)
-
-            log_seek(pmo, pmo_header[11] + (mesh_header[5] + vertex_group_header[0]) * 16, "vertex_data", 2)
-
-            # material = mats[i]
-
-            ge_extra_indent = 0 if enforce_ge_verbose else 3
-            ge_verbose = verbose or enforce_ge_verbose
-
-            if has_dbg_ge_once:
-                ge_verbose = False
-
-            if second:
-                second.seek(vertex_group_header[3])
-                mesh.append(run_ge(second, scale, ge_verbose, ge_extra_indent) + (material,))
-                has_dbg_ge_once = True
-                Logger.enable = verbose
-            else:
-                raw = vertex_group_header[3]
-                Logger.debug(f"Raw (before offset): {raw}", indent=2)
-                offset = pmo_header[12] + raw
-                test_offset = pmo_header[12] + raw
-                Logger.debug(f"{pmo_header[12]:08X} + {raw:08X} = ", indent=2)
-                Logger.debug(f"Offset1: {offset:08X}", indent=2)
-                Logger.highlight(f" Offset2: {test_offset:08X}", indent=2, color=LogStyle.YELLOW if offset != test_offset else LogStyle.BRIGHT_BLACK)
-
-                Logger.highlight("Vertex Data (GE   ): ", indent=2, color=LogStyle.GREEN)
-
-                if offset > pmo_header[0]:
-                    Logger.highlight(f"Offset TOO LARGE, abort!", indent=2, color=LogStyle.RED)
-                    break
-
-                log_seek(pmo, offset, "vertex_data", 2)
-                try:
-                    result = run_ge(pmo, scale, ge_verbose, ge_extra_indent)
-                    has_dbg_ge_once = True
-                    Logger.enable = verbose
-
-                    if not result:
-                        Logger.warn(f"Empty GE at mesh {i}, group {count}", 1)
-                        break
-                    
-                    mesh.append(result + (material,))
-                except Exception as e:
-                    Logger.warn(f"Stopping mesh {i}, group {count}: {e}", 1)
-                    break
-            count += 1
-
-        if mesh:
-            offsets = {'v': 1, 'vt': 1, 'vn': 1}
-            new_save_path = os.path.join(dirname, basename.replace(".obj", f"-{i}.obj"))
-            with open(new_save_path, 'w') as o:
-                o.write('mtllib {}\n'.format(mtl_file))
-                o.write(f'g mesh{i:02d}\n')
-                create_mesh(o, offsets, mesh)
-            place_mtl_in_obj(new_save_path, "material.mtl")
-
 def determine_i_len(pmo, pmo_header):
     vertex_raw_bytes = int((pmo_header[9] - pmo_header[8]))
     pmo.seek(pmo_header[8])
@@ -304,11 +224,10 @@ def convert_mh2_pmo(pmo, obj, mtl_file, second=None, verbose=False, enforce_ge_v
     basename = os.path.basename(obj.name)
     count = 0
 
-    # print((pmo_header[9] - pmo_header[8]) / 16)
-    # print(pmo_header[6])
-
+    number_of_mesh = determine_i_len(pmo, pmo_header)
     mesh_header = read_mesh_header(pmo, pmo_header[7], 1)
-    for i in range(determine_i_len(pmo, pmo_header)):
+
+    for i in range(number_of_mesh):
         Logger.highlight(f"I-Loop ({i+1} / {pmo_header[6]})", 0, color=LogStyle.BRIGHT_MAGENTA)
         mesh = []
 
@@ -321,29 +240,29 @@ def convert_mh2_pmo(pmo, obj, mtl_file, second=None, verbose=False, enforce_ge_v
             vertex_group_header = read_vertex_group(pmo, vg_offset, 2)
 
             pmo.seek(pmo_header[9])
-            indices_num = 16*int(pmo_header[6]/16+1)  # Some of them are trailing zeros, don't count
+            indices_num = 16*int(number_of_mesh/16+1)  # Some of them are trailing zeros, don't count
             
             mat_indices = struct.unpack(f"{indices_num}B", pmo.read(indices_num))
-            # print(mat_indices)
             mat_index = mat_indices[vertex_group_header[0]]
-
             pmo.seek(pmo_header[11] + (mat_index) * 16)
+            # !!! This is not the correct way to get material index
+            # Do this for now as I have no clue how to get it +
+            # it is not that important to me
             material = struct.unpack('4I', pmo.read(16))[2]
 
-            new_val = vertex_group_header[0]
-            # log_seek(pmo, pmo_header[11] + (mesh_header[5] + vertex_group_header[0]) * 16, "vertex_data", 2)
+            flag = vertex_group_header[0]
 
+            # ==================================
+            # Not important debug info
             ge_extra_indent = 0 if enforce_ge_verbose else 3
             ge_verbose = verbose or enforce_ge_verbose
-
             if has_dbg_ge_once:
                 ge_verbose = False
             ge_verbose = False
-            
-            # print(vertex_group_header[0], mat_index, material)
+            # ==================================
 
-            if len(vtx_idx) == 0 or new_val == vtx_idx[-1]:
-                vtx_idx.append(new_val)
+            if len(vtx_idx) == 0 or flag == vtx_idx[-1]:
+                vtx_idx.append(flag)
                 if second:
                     second.seek(vertex_group_header[3])
                     mesh.append(run_ge(second, scale, ge_verbose, ge_extra_indent) + (material,))
